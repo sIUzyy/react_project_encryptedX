e491db3db47832f839e09549f47dfbf0b1eef399
Justin Peligro <122611993+sIUzyy@users.noreply.github.com>
2023-05-10 11:51:30 +0800

    Update Landing.jsx

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,24 +1,15 @@
+//landing page...
+
 import React from 'react'
 import mainImg from '../assets/landing-img.webp'
 
 const Landing = () => {
   return (
-  //   <div className='bg-white text-black mt-10 mb-0 '>
-
-  //       <img className='w-full' src={mainImg} alt='' />
-        
-  //   <div className='mx-auto max-w-7xl px-6 lg:px-8'>
-  //   <h1>dsa</h1>
-    
-
-
-
-  //   </div>
   
-  
-  // </div>
   <div id='home' className='bg-white text-black mt-10 mb-0 relative'>
-  <img className='w-full blur md:blur-md' src={mainImg} alt='' />
+    <div className='h-72 md:h-auto'>
+      <img className='w-full h-72 blur md:blur-md md:h-auto' src={mainImg} alt='' />
+    </div>
   <div className='absolute top-10 left-0 right-0 bottom-0 flex items-center justify-center'>
     <div className='text-center text-white uppercase font-landing'>
       <h1 className='text-2xl text-[#E2DE84] md:text-6xl'>Welcome to Encryptedx </h1>
